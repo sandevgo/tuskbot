c5a2174d35b2d8363d36f565a66d7af9fb240ae6
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-17 23:13:06 +0300

    feat(llamacpp/sqlite): update CFLAGS and LDFLAGS for Linux ARM64
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

--- pkg/llamacpp/flags_linux_arm64.go
+++ pkg/llamacpp/flags_linux_arm64.go
@@ -1,6 +1,5 @@
 package llamacpp
 
-/*
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lllama -lggml -lstdc++ -lm -fopenmp
-*/
+// #cgo CFLAGS: -I${SRCDIR}/include
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lllama -lggml -lstdc++ -lm -fopenmp
 import "C"
--- pkg/sqlite/flags_linux_arm64.go
+++ pkg/sqlite/flags_linux_arm64.go
@@ -1,6 +1,5 @@
 package sqlite
 
-/*
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lsqlite3 -lsqlite3_vec -lm
-*/
+// #cgo CFLAGS: -I${SRCDIR}/include
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lsqlite3 -lsqlite3_vec -lm
 import "C"
