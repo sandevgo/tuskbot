9032fe0d32ab1f0cbdca6edb4ee99dbe5e23e165
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-17 23:30:48 +0300

    fix(linux_arm64): update C flags for llama library
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

--- pkg/llamacpp/flags_linux_arm64.go
+++ pkg/llamacpp/flags_linux_arm64.go
@@ -1,5 +1,7 @@
+//go:build linux && arm64
+
 package llamacpp
 
 // #cgo CFLAGS: -I${SRCDIR}/include
-// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lllama -lggml -lstdc++ -lm -fopenmp
+// #cgo LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lllama -lggml -lggml-cpu -lggml-base -lc++ -lm
 import "C"
