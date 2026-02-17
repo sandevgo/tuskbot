92f1e9158a00453e89819cc650f75285372500ec
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-17 22:53:57 +0300

    feat(golang): add flags for Linux ARM64 libraries
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

---
+++ pkg/llamacpp/flags_linux_arm64.go
@@ -0,0 +1,6 @@
+package llamacpp
+
+/*
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lllama -lggml -lstdc++ -lm -fopenmp
+*/
+import "C"
---
+++ pkg/sqlite/flags_linux_arm64.go
@@ -0,0 +1,6 @@
+package sqlite
+
+/*
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lsqlite3 -lsqlite3_vec -lm
+*/
+import "C"
