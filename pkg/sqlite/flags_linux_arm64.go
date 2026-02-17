68fdb4c3c9940a708ddfab27adc3bec2118609e5
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-17 23:25:26 +0300

    fix(sqlite): linux arm64 flags
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

--- pkg/sqlite/flags_linux_arm64.go
+++ pkg/sqlite/flags_linux_arm64.go
@@ -1,5 +1,7 @@
+//go:build linux && arm64
+
 package sqlite
 
 // #cgo CFLAGS: -I${SRCDIR}/include
-// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lsqlite3_vec -lm
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib/linux_arm64 -lsqlite_vec -lm
 import "C"
