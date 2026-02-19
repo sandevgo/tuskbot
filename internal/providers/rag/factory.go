758e71637c7dc7704f58dae336b3e7544eaffffc
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-20 01:42:04 +0300

    feat(internal/providers/rag/factory): add support for embedding models using LlamaCpp
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

---
+++ internal/providers/rag/factory.go
@@ -0,0 +1,25 @@
+package rag
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/sandevgo/tuskbot/internal/config"
+	"github.com/sandevgo/tuskbot/pkg/llamacpp"
+)
+
+func NewEmbeddingModel(cfg config.RAGConfig) (DualEncoder, error) {
+	modelPath := filepath.Join(config.GetRuntimePath(), "models", cfg.ModelName)
+
+	llamaEmb, err := llamacpp.NewLlamaEmbedder(modelPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load embedding model: %w", err)
+	}
+
+	switch cfg.ModelName {
+	case ModelNameE5BaseQ8:
+		return NewE5BaseModel(llamaEmb), nil
+	default:
+		return nil, fmt.Errorf("unknown model name: %s", cfg.ModelName)
+	}
+}
