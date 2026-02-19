134705bfd3be5997c1dbbb7ad9dbdd75b2808249
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-19 03:18:25 +0300

    feat(llamacpp): haswell support
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

--- pkg/llamacpp/flags_linux_amd64.go
+++ pkg/llamacpp/flags_linux_amd64.go
@@ -2,6 +2,6 @@
 
 package llamacpp
 
-// #cgo CFLAGS: -I${SRCDIR}/include
+// #cgo CFLAGS: -I${SRCDIR}/include -O3 -march=haswell
 // #cgo LDFLAGS: -L${SRCDIR}/lib/linux_amd64 -lllama -lggml -lggml-cpu -lggml-base -lc++ -lm
 import "C"
