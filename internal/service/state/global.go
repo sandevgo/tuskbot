402d7910e9e2b89d4c4925c8269edc2aca4ab4ae
Aleksandr Golenishchev <git@golenishchev.com>
2026-02-23 19:06:54 +0300

    fix(state/global): update function to use new writer interface
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

--- internal/service/state/global.go
+++ internal/service/state/global.go
@@ -17,10 +17,10 @@ func NewGlobalState(
 	writer configWriter,
 ) *GlobalState {
 	return &GlobalState{
-		config: configWriter,
+		writer: writer,
 	}
 }
 
 func (s *GlobalState) ChangeModel(ctx context.Context, model string) error {
-	return s.config.SetModel(model)
+	return s.writer.SetModel(model)
 }
