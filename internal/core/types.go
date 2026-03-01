d983baff0671870e89e9327b266327517ac29086
Aleksandr Golenishchev <git@golenishchev.com>
2026-03-01 17:29:40 +0300

    feat(core): add Tool, ToolCall, and FunctionCall structs
    Signed-off-by: Aleksandr Golenishchev <git@golenishchev.com>

--- internal/core/types.go
+++ internal/core/types.go
@@ -1,9 +1,5 @@
 package core
 
-import (
-	"encoding/json"
-)
-
 const (
 	RoleUser      = "user"
 	RoleAssistant = "assistant"
@@ -17,28 +13,6 @@ const (
 	TuskRepositoryURL = "https://github.com/sandevgo/tuskbot"
 	TaskVersion       = "0.1.0"
 )
-
-type Function struct {
-	Name        string          `json:"name"`
-	Description string          `json:"description,omitempty"`
-	Parameters  json.RawMessage `json:"parameters"` // JSON Schema
-}
-
-type Tool struct {
-	Type     string   `json:"type"`
-	Function Function `json:"function"`
-}
-
-type ToolCall struct {
-	ID       string       `json:"id"`
-	Type     string       `json:"type"`
-	Function FunctionCall `json:"function"`
-}
-
-type FunctionCall struct {
-	Name      string `json:"name"`
-	Arguments string `json:"arguments"`
-}
 
 type Message struct {
 	Role       string     `json:"role"`
